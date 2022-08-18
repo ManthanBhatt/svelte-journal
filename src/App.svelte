b2431cad2b8cdd36d16a20a06cf4b6ec01c670e9
Manthan Bhatt <manthan@markettime.com>
2022-08-18 10:34:29 +0530

    Implemented ts and added few examples

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,13 @@
-<script>
-	let name = 'world';
+<script lang="ts">
+	import Child from "./component/Child.svelte";
+	const name:string = 'world';
 </script>
 
+<style>
+	h1 {
+		color: purple;
+	}
+</style>
+
 <h1>Hello {name}!</h1>
+<Child></Child>
---
+++ src/component/Child.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  const name: string = "child component";
+  let count = 0;
+  const hitMe = () => count++;
+</script>
+
+<p>Hello from {name}.</p>
+<button on:click={hitMe}>Hit me {count}</button>
